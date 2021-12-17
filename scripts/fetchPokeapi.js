0a6e7486cb8031f0032226cdb5327da41ea3dad8
Mauricio2802 <vmcarrasco2810@gmail.com>
2021-12-17 13:50:32 -0500

    Change the algorithm to load faster

--- scripts/fetchPokeapi.js
+++ scripts/fetchPokeapi.js
@@ -1,3 +1,7 @@
+const pokedex = document.getElementById("pokedex"),
+  pokedexChilds = Array.from(pokedex.children),
+  pokedexLength = pokedexChilds.length;
+
 // const colors = {
 //   normal: "#A8A77A",
 //   fire: "#EE8130",
@@ -19,36 +23,77 @@
 //   fairy: "#D685AD",
 // };
 
-function fetchData(url_api) {
-  return fetch(url_api).then(response => response.json());
-}
-function capitalizeString(string) {
-  const stringCapitalized = string.charAt(0).toUpperCase() + string.slice(1);
-  return stringCapitalized;
-}
-
 async function consultPokemon(id) {
   try {
     const pokemon = await fetchData(`https://pokeapi.co/api/v2/pokemon/${id}`);
     // console.log(pokemon);
-    createPokemon(pokemon);
+    createPokemon(pokemon, id);
   } catch {
     console.error(error);
   }
 }
 
 async function consultPokemons(numPokemons) {
-  let index = 0;
   for (i = 0; i < numPokemons; i++) {
     await consultPokemon(i + 1);
   }
 }
 
-consultPokemons(151);
+window.onload = consultPokemons(pokedexLength);
 
-const pokedex = document.getElementById("pokedex");
+function createPokemon(pokemon, id) {
+  let thisPokemon = pokedexChilds[id - 1];
+  let itemId = thisPokemon.querySelector(".pokemon__id");
+  let itemImg = thisPokemon.querySelector(".pokemon__img");
+  let itemName = thisPokemon.querySelector(".pokemon__name");
+  let itemTypeCtn = thisPokemon.querySelector(".pokemon__type");
 
-function createPokemon(pokemon) {
+  itemId.textContent = `#${elegantId(pokemon.id)}`;
+
+  itemImg.setAttribute(
+    "src",
+    `${pokemon.sprites.other.dream_world.front_default}`
+  );
+  if (itemImg.getAttribute("src") == "null") {
+    itemImg.setAttribute("src", `${pokemon.sprites.other.home.front_default}`);
+  }
+
+  itemName.textContent = `${capitalizeString(pokemon.species.name)}`;
+
+  let type = document.createElement("div");
+  itemTypeCtn.appendChild(type);
+  let type1 = pokemon.types[0].type.name;
+  type.textContent = `${capitalizeString(type1)}`;
+  backgroundType(type);
+
+  let type2;
+  if (pokemon.types[1]) {
+    let type = document.createElement("div");
+    itemTypeCtn.appendChild(type);
+    type2 = pokemon.types[1].type.name;
+    type.textContent = `${capitalizeString(type2)}`;
+    backgroundType(type);
+  }
+
+  let totalLength;
+  if (type2) {
+    totalLength = type1.length + type2.length;
+  } else {
+    totalLength = type1.length;
+  }
+
+  if (totalLength >= 12) {
+    itemTypeCtn.style.padding = "0";
+  }
+  if (totalLength >= 15) {
+    let arrayTypes = Array.from(itemTypeCtn.children);
+    for (type of arrayTypes) {
+      type.style.padding = "4px 8px";
+    }
+  }
+}
+
+function createNewPokemonItem(pokemon) {
   let itemContainer = document.createElement("li");
   let itemBg = document.createElement("div");
   let item = document.createElement("div");
@@ -86,8 +131,6 @@ function createPokemon(pokemon) {
   itemTypeCtn.appendChild(type);
   let type1 = pokemon.types[0].type.name;
   itemTypeCtn.children[0].textContent = `${capitalizeString(type1)}`;
-  // console.log(itemTypeCtn.children[0].textContent.toLowerCase());
-  // console.log(itemTypeCtn.children[0].textContent);
   backgroundType(itemTypeCtn.children[0]);
 
   let type2;
@@ -109,54 +152,4 @@ function createPokemon(pokemon) {
     // console.log(`${pokemon.name} ${type1}, ${type2}, ${totalLength}`);
     itemTypeCtn.style.padding = "0";
   }
-}
-
-function backgroundType(type) {
-  if (type.textContent.toLowerCase() === "normal") {
-    type.classList.add("fire");
-  } else if (type.textContent.toLowerCase() === "fighting") {
-    type.classList.add("fighting");
-  } else if (type.textContent.toLowerCase() === "flying") {
-    type.classList.add("flying");
-  } else if (type.textContent.toLowerCase() === "poison") {
-    type.classList.add("poison");
-  } else if (type.textContent.toLowerCase() === "ground") {
-    type.classList.add("ground");
-  } else if (type.textContent.toLowerCase() === "rock") {
-    type.classList.add("rock");
-  } else if (type.textContent.toLowerCase() === "bug") {
-    type.classList.add("bug");
-  } else if (type.textContent.toLowerCase() === "ghost") {
-    type.classList.add("ghost");
-  } else if (type.textContent.toLowerCase() === "steel") {
-    type.classList.add("steel");
-  } else if (type.textContent.toLowerCase() === "fire") {
-    type.classList.add("fire");
-  } else if (type.textContent.toLowerCase() === "water") {
-    type.classList.add("water");
-  } else if (type.textContent.toLowerCase() === "grass") {
-    type.classList.add("grass");
-  } else if (type.textContent.toLowerCase() === "electric") {
-    type.classList.add("electric");
-  } else if (type.textContent.toLowerCase() === "psychic") {
-    type.classList.add("psychic");
-  } else if (type.textContent.toLowerCase() === "ice") {
-    type.classList.add("ice");
-  } else if (type.textContent.toLowerCase() === "dragon") {
-    type.classList.add("dragon");
-  } else if (type.textContent.toLowerCase() === "dark") {
-    type.classList.add("dark");
-  } else if (type.textContent.toLowerCase() === "fairy") {
-    type.classList.add("fairy");
-  }
-}
-
-function elegantId(id) {
-  let elegantId = String(id);
-  if (elegantId.length === 1) {
-    elegantId = `00${elegantId}`;
-  } else if (elegantId.length === 2) {
-    elegantId = `0${elegantId}`;
-  }
-  return elegantId;
 }
---
+++ scripts/utilsFunctions.js
@@ -0,0 +1,57 @@
+function fetchData(url_api) {
+  return fetch(url_api).then(response => response.json());
+}
+function capitalizeString(string) {
+  const stringCapitalized = string.charAt(0).toUpperCase() + string.slice(1);
+  return stringCapitalized;
+}
+
+function backgroundType(type) {
+  if (type.textContent.toLowerCase() === "normal") {
+    type.classList.add("fire");
+  } else if (type.textContent.toLowerCase() === "fighting") {
+    type.classList.add("fighting");
+  } else if (type.textContent.toLowerCase() === "flying") {
+    type.classList.add("flying");
+  } else if (type.textContent.toLowerCase() === "poison") {
+    type.classList.add("poison");
+  } else if (type.textContent.toLowerCase() === "ground") {
+    type.classList.add("ground");
+  } else if (type.textContent.toLowerCase() === "rock") {
+    type.classList.add("rock");
+  } else if (type.textContent.toLowerCase() === "bug") {
+    type.classList.add("bug");
+  } else if (type.textContent.toLowerCase() === "ghost") {
+    type.classList.add("ghost");
+  } else if (type.textContent.toLowerCase() === "steel") {
+    type.classList.add("steel");
+  } else if (type.textContent.toLowerCase() === "fire") {
+    type.classList.add("fire");
+  } else if (type.textContent.toLowerCase() === "water") {
+    type.classList.add("water");
+  } else if (type.textContent.toLowerCase() === "grass") {
+    type.classList.add("grass");
+  } else if (type.textContent.toLowerCase() === "electric") {
+    type.classList.add("electric");
+  } else if (type.textContent.toLowerCase() === "psychic") {
+    type.classList.add("psychic");
+  } else if (type.textContent.toLowerCase() === "ice") {
+    type.classList.add("ice");
+  } else if (type.textContent.toLowerCase() === "dragon") {
+    type.classList.add("dragon");
+  } else if (type.textContent.toLowerCase() === "dark") {
+    type.classList.add("dark");
+  } else if (type.textContent.toLowerCase() === "fairy") {
+    type.classList.add("fairy");
+  }
+}
+
+function elegantId(id) {
+  let elegantId = String(id);
+  if (elegantId.length === 1) {
+    elegantId = `00${elegantId}`;
+  } else if (elegantId.length === 2) {
+    elegantId = `0${elegantId}`;
+  }
+  return elegantId;
+}
